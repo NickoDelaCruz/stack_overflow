4e0609c8c66cdf68581240e6874869cafd17ef79
Nicko Dela Cruz and Ron Craig <ron@>
2018-04-24 12:19:19 -0700

    added new and create functions in Comments Controller Nicko

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,5 +8,20 @@
       @post = Comment.post
     end
 
+    def new
+      @post = Post.find(params[:post_id])
+      @comments = @post.comments.new
+    end
+
+    def create
+      @post = Post.find(params[:post_id])
+      @comment = @post.comments.new(params)
+      if @comment.save
+        flash[:notice] = "Comment Successfully added"
+        redirect_to post_path(@comment.post)
+      else
+        render :new
+      end
+    end
 
   end
