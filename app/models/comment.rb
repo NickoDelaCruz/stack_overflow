b3f6ffdfa1ac21c5a2fdc2beddf8d5437a5b4ba0
Nicko Dela Cruz and Ron Craig <nicko+ron@>
2018-04-24 13:30:20 -0700

    add errors to fix error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   validates :username, :presence => true
   validates :comment_body, :presence => true, :length => { :minimum => 20, :maximum => 250}
-  # validates :post_id, :presence => true
+  validates :post_id, :presence => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   attr_accessor :password
-  # validates_confirmation_of :password
+  validates_confirmation_of :password
   validates :email, :presence => true, :uniqueness => true
   before_save :encrypt_password
 
