ad01e9a3d49dbfc9c3cfe1af57d51e2dfa3c641d
Nicko Dela Cruz and Ron Craig <ron@>
2018-04-24 12:34:23 -0700

    add Comment validations in models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
+  belongs_to :post
   validates :username, :presence => true
-  validates :comment_body, :presence => true
+  validates :comment_body, :presence => true, :length => { :minimum => 20, :maximum => 250}
+  validates :post_id, :presence => true
 end
